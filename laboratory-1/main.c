916ef9726adcff325d785dfb60f745bba4bdfad1
Roman Yatman <machlestv@gmail.com>
2021-09-08 19:02:35 +0700

    init commit laboratory-1 and laboratory-2 are ready

--- laboratory-1/main.c
+++ laboratory-1/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <thread_db.h>
+#include <stdlib.h>
+
+#define THREADS_NUMBER 2
+#define STRINGS_NUMBER 10
+
+void * writeStrings(void* arg){
+    int strNumber = (int) arg;
+
+    for (int i = 1; i <= strNumber; ++i) {
+        printf("Children text: %d\n", i);
+    }
+
+    return NULL;
+}
 
 int main() {
-    fork();
-    printf("Hello, World!\n");
-    return 0;
+    thread_t tid[THREADS_NUMBER];
+    int executionStatus = pthread_create(&tid[0], NULL, writeStrings, (void*) STRINGS_NUMBER);
+
+    if(executionStatus != 0){
+        perror("There are problems with creating thread.");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 1; i <= STRINGS_NUMBER; ++i) {
+        printf("Parent text: %d\n", i);
+    }
+
+    return EXIT_SUCCESS;
 }
--- laboratory-3/main.c
+++ laboratory-3/main.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <thread_db.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define THREADS_NUMBER 4
+#define STRINGS_NUMBER 3
+#define MAX_STRING_LENGTH 100
+
+char** initStringsForPrinting(char ** strings, int threadNumber){
+    strings[THREADS_NUMBER][MAX_STRING_LENGTH];
+    for (int i = 0; i < THREADS_NUMBER; ++i) {
+        strings[i] = strncat("I'm strings printed by thread N", (const char *) threadNumber, 1);
+    }
+
+    return strings;
+}
+
+void * writeStrings(void* arg){
+    char** stringsForPrint = (char**) arg;
+
+    printf("Children text: %s\n", arg);
+
+    return NULL;
+}
 
 int main() {
-    fork();
-    printf("Hello, World!\n");
-    return 0;
+    thread_t tid[THREADS_NUMBER];
+    int executionStatus;
+
+    for (int i = 0; i < THREADS_NUMBER; ++i) {
+
+    }
+
+    for (int i = 0; i < THREADS_NUMBER; ++i) {
+        executionStatus = pthread_create(&tid[i], NULL, writeStrings, (void*) i);
+
+        if(executionStatus != 0){
+            perror("There are problems with creating thread.");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 0; i < THREADS_NUMBER; ++i) {
+        executionStatus = pthread_join(tid[i], NULL);
+
+        if(executionStatus != 0){
+            perror("There are problems with joining thread.");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
